126671a1cb306d1274677fbc4cd3585ba4ad9460
poetrr <sidducit012@gmail.com>
2025-04-22 01:15:26 +0530

    made some changes

--- src/main/java/com/AttendanceTracker/Prelag/Controller/SubjectsController.java
+++ src/main/java/com/AttendanceTracker/Prelag/Controller/SubjectsController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.AttendanceTracker.Prelag.DTOS.AlterSubjectsRequest;
 import com.AttendanceTracker.Prelag.DTOS.CreateSubjectsRequest;
-import com.AttendanceTracker.Prelag.DTOS.SemesterDTO;
+
 import com.AttendanceTracker.Prelag.DTOS.UpdateSubjectRequest;
 import com.AttendanceTracker.Prelag.Model.Subject;
 import com.AttendanceTracker.Prelag.Service.JwtUtil;
